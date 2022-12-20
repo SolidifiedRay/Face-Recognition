6d3589ce9a6ce8f943554d80cb0422977936549d
SolidifiedRay <yc3346@nyu.edu>
2022-12-19 22:35:18 -0500

    formatting code

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 import math
 
-
 def main():
   trainingImagesPath = [
     ".\dataset\Training\subject01.happy.jpg",
@@ -29,41 +28,42 @@ def main():
   columnVectors = get_column_vector_set(trainingImagesPath)
   # The mean face is computed by taking the average of the training face images
   meanFace = np.mean(columnVectors, axis=0)
-  show_mean_face(meanFace, imageRow, imageColumn)
+  show_save_mean_face(meanFace, imageRow, imageColumn)
   # We subtract the mean face from each training face and put them into a single matrix A
-  # The follow function return A transpose
   A = get_normalized_column_vector_set(columnVectors, meanFace)
-  AT = A.transpose()
   # We find eigenvalues of L = A transpose x A
-  L = np.matmul(AT, A)
+  L = np.matmul((A.T), A)
   # Put eigenvectors of L into a single matrix V
   _, V = np.linalg.eig(L)
   # The M largest eigenvectors of C can be found by U = AV
   U = np.matmul(A,V)
-  U = normalize_U(U)
-  UT = U.transpose()
-  # Display eigenface
-  show_eigenface(UT, imageRow, imageColumn)
-  # For each training face, calculate its eigenface coefficients omega
+  # Display and save eigenface
+  show_save_eigenface(U.T, imageRow, imageColumn)
+  # For each training faces, calculate its eigenface coefficients
   trainingCoefficients = np.zeros(shape=(len(trainingImagesPath),len(trainingImagesPath)))
   for i in range(len(trainingImagesPath)):
-    omega = np.matmul(UT, A[:,i])
-    trainingCoefficients[i]=omega
+    trainingCoefficients[i]=np.matmul(U.T,A[:,i])
+  print("The Eigenface coefficients of the training images:")
+  print(trainingCoefficients)
+  print()
+
+  # ----------------------------------- Face recognition -----------------------------------
+
   # Calculate the eigenface coefficient of each testing image
   testingColumnVectors = get_column_vector_set(testingImagesPath)
   testingNormalizedColumnVectors = get_normalized_column_vector_set(testingColumnVectors, meanFace)
   testingCoefficients = np.zeros(shape=(len(testingImagesPath),len(trainingImagesPath)))
   for i in range(len(testingImagesPath)):
-    omega = np.matmul(UT, testingNormalizedColumnVectors[:,i])
-    print(omega)
+    omega = np.matmul(U.T, testingNormalizedColumnVectors[:,i])
     testingCoefficients[i]=omega
+  print("The Eigenface coefficients of the testing images:")
+  print(testingCoefficients)
+  print()
 
   # Calculate and print recognition result for each test image
   for i in range(len(testingCoefficients)):
-    print(testingImagesPath[i] + " is match to ")
     matchIndex = get_match_face(testingCoefficients[i], trainingCoefficients)
-    print(trainingImagesPath[matchIndex])
-    print()
+    print(testingImagesPath[i][18:] + " is match to " + trainingImagesPath[matchIndex][19:])
 
 
 def get_column_vector_set(trainingImagesPath):
@@ -79,6 +79,7 @@ def get_column_vector_set(trainingImagesPath):
       column_vector_set = np.append(column_vector_set,image,axis=0)
   return column_vector_set
 
+
 def get_normalized_column_vector_set(columnVectors, meanFace):
   # normalize column vectors by subtracting the average face from them
   normalize_cv = np.ndarray(shape=(len(columnVectors),45045))
@@ -86,17 +87,6 @@ def get_normalized_column_vector_set(columnVectors, meanFace):
     normalize_cv[i] = columnVectors[i] - meanFace
   return normalize_cv.T
 
-def normalize_U(U):
-  # normalize to 0-255
-  Umin=np.amin(U)
-  Umax=np.amax(U)
-  U2=np.ndarray(shape=(45045,8))
-  for i in range(U.shape[0]):
-    for j in range(U.shape[1]):
-      g = U[i][j]
-      g = (g - Umin) * 255/(Umax - Umin)
-      U2[i][j]=g
-  return U2
 
 def get_euclidean_distance(a, b):
   # calculate euclidean distance between two vectors
@@ -126,23 +116,25 @@ def show_image(image, name = "image"):
     cv2.destroyAllWindows()
 
 
-def show_mean_face(meanFace, imageRow, imageColumn):
+def show_save_mean_face(meanFace, imageRow, imageColumn):
   # the dimension of meanFace is N^2 x 1
   print("Displaying mean face")
   print("Press 0 to close the image")
   print()
   image = np.uint8(np.reshape(meanFace, (imageRow, imageColumn)))
+  cv2.imwrite("mean face.bmp",image)
   show_image(image)
 
 
-def show_eigenface(UT, imageRow, imageColumn):
+def show_save_eigenface(UT, imageRow, imageColumn):
   i = 1
   for eigenface in UT:
     print("Displaying eigenface ", i)
-    i += 1
     print("Press 0 to close the images")
     print()
     image = np.uint8(np.reshape(eigenface, (imageRow, imageColumn)))
+    cv2.imwrite("egienface"+str(i)+".bmp",image)
+    i += 1
     show_image(image)
 
 
